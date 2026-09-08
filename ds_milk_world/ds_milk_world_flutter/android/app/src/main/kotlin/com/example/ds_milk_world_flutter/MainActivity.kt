d39d7550b7964229e4593efdb23d6737ce0c1ec8
dasara-varun <varun.dasara2405@gmail.com>
2026-09-08 16:58:35 +0530

    feat(scaffold): initialize Serverpod 3.x and Flutter project workspace

---
+++ ds_milk_world/ds_milk_world_flutter/android/app/src/main/kotlin/com/example/ds_milk_world_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ds_milk_world_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ds_milk_world/ds_milk_world_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:ds_milk_world_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
